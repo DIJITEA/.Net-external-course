4b2193cb11eca20410a709c1cfc45b9def2385c8
Oleg <Kornyakovo@gmail.com>
2021-12-29 15:36:46 +0400

    feat: add "sum of numbers" task

---
+++ Task 1/1.1.5_Sum_of_numbers/Sum_of_numbers.cs
@@ -0,0 +1,24 @@
+﻿namespace _1._1._5_Sum_of_numbers
+{
+    public class SumOfNubers
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Sum of nubers");
+            int sum = 0;
+            int goLength = 1000;
+            for (int i = 3; i < goLength; i += 3)
+            {
+                sum = sum + i;            
+            }
+            for (int j = 5; j < goLength; j += 5)
+            {
+                if ( j % 3 != 0)
+                {
+                    sum = sum + j;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
