6f4e9d8c48ee79631e4d93def067cb0cf03fd4e8
Oleg <Kornyakovo@gmail.com>
2022-01-02 21:20:32 +0400

    feat: add "no possitive" task

---
+++ Task 1/1.1.8_No_positive/No_positive.cs
@@ -0,0 +1,28 @@
+﻿using used_frequently;
+
+namespace _1._1._8_No_positive
+{
+    public class NoPositive
+    {
+        public static void Main()
+        {
+            Console.WriteLine("1.1.8 No positive");
+            Console.Write("enter array (n*n*n)length: ");
+            int n = UsedFrequently.Try2Parse();
+            int[,,] TArray = new int[n,n,n];
+
+            for (int i = 0; i < TArray.GetLength(0); i++)
+            {
+                for (int j = 0; j < TArray.GetLength(1); j++)
+                {
+                    for (int  k = 0;   k< TArray.GetLength(1);  k++)
+                    {
+                        TArray[i, j, k] = new Random().Next(-100, 100);
+                        TArray[i, j, k] = TArray[i, j, k] > 0 ? 0 : TArray[i, j, k];
+                        Console.WriteLine(TArray[i, j, k]);
+                    }
+                }
+            }
+        }
+    }
+}
