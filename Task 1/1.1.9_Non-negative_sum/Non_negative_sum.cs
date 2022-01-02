0af67cbce5b256b3dbe5bfdbff2f95003b6cdc8b
Oleg <Kornyakovo@gmail.com>
2022-01-02 21:21:01 +0400

    feat: add "non-negative sum" task

---
+++ Task 1/1.1.9_Non-negative_sum/Non_negative_sum.cs
@@ -0,0 +1,22 @@
+﻿using used_frequently;
+
+namespace _1._1._9_Non_negative_sum
+{
+    public class NonNegativeSum
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Non-Negative Sum");
+            Console.Write("enter array length: ");
+            int n = UsedFrequently.Try2Parse();
+            int[] Array = UsedFrequently.ArrayRandomize(n);
+            int sum = 0;
+            Console.WriteLine($"Unsorted Array: {String.Join(" ", Array)}");
+            foreach (int i in Array)
+            {
+              sum += i > 0 ? i : 0;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
