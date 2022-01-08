92b6a1dcb12ce490b6a4788b374e8baf8b7967a6
Oleg <Kornyakovo@gmail.com>
2022-01-08 17:34:12 +0400

    feat: add "Lowercase" task

---
+++ Task 1/1.2.3_LowerCase/Lowercase.cs
@@ -0,0 +1,32 @@
+﻿namespace _1._2._3_LowerCase
+{
+    public class Lowercase
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Lowercase");
+            string inputStr = Console.ReadLine();
+            if (!String.IsNullOrEmpty(inputStr))
+            {
+                inputStr = System.Text.RegularExpressions.Regex.Replace(inputStr, @"[^\w]", " ");
+                int counter = 0;
+                if (Char.IsLower(inputStr[0]))
+                {
+                    counter++;
+                }
+                for (int i = 1; i < inputStr.Length; i++)
+                {
+                    if (inputStr[i - 1] == ' ' && Char.IsLower(inputStr[i]))
+                    {
+                        counter++;
+                    }
+                }
+
+                Console.WriteLine(counter);
+            }   else
+            {
+                Console.WriteLine("Error: string is empty or null");
+            }
+        }
+    }
+}
