e73194b9f9175c548199ce5c1a1fe6ce87b3490a
Oleg <Kornyakovo@gmail.com>
2022-01-08 17:35:11 +0400

    feat: add "Validator" task

---
+++ Task 1/1.2.4_Validator/Validator.cs
@@ -0,0 +1,39 @@
+﻿namespace _1._2._4_Validator
+{
+    public class Validator
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Validator");
+            string inputStr = Console.ReadLine();
+            if (!String.IsNullOrEmpty(inputStr))
+            {
+                string outputStr = inputStr[0].ToString().ToUpper();
+
+                for (int i = 1; i < inputStr.Length; i++)
+                {
+                    if (inputStr[i] == '.' || inputStr[i] == '?' || inputStr[i] == '!' && inputStr[i] != inputStr[inputStr.Length - 1])
+                    {
+                        outputStr = outputStr + inputStr[i];
+                        i++;
+                        while (inputStr[i] == ' ')
+                        {
+                            outputStr = outputStr + inputStr[i];
+                            i++;
+                        }
+                        outputStr = outputStr + (inputStr[i].ToString().ToUpper());
+                    }
+                    else
+                    {
+                        outputStr = outputStr + inputStr[i];
+                    }
+                }
+                Console.WriteLine(outputStr);
+            }   else
+            {
+                Console.WriteLine("Error: string is empty or null");
+            }
+        }
+
+    }
+}
