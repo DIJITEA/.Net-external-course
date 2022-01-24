84781c93dd9464b0a5f0994330c289c3a7c62157
Oleg <Kornyakovo@gmail.com>
2022-01-25 03:41:48 +0400

    feat: finished task 2.2

--- Task 2/Task_2.2/Game_Development/Characters/Class1.cs
+++ Task 2/Task_2.2/Game_Development/Characters/Class1.cs
@@ -7,6 +7,14 @@
         public static int[] playerPosition { get { return _playerPosition; } set { _playerPosition = value; } }
         protected static int[] _fieldBorder = new int[2];
         
+        public static bool isHiden() 
+        {
+            if ( Tree.forest[playerPosition[0], Tree.playerPosition[1]] == 1 )
+                return true;
+            else 
+                return false;
+        }
+
         // for if?
         public static void setborder(int[] setArray)
         {
@@ -22,19 +30,35 @@
             switch (directionSwitch)
             {
                 case 1:
-                    _position[0] = _position[0] + 1;
-                    break;
+                    if (_position[0] + 1 < _fieldBorder[0] - 1)
+                    {
+                        _position[0] = _position[0] + 1;
+                        break;
+                    }   else
+                        break;
                 case 2:
-                    _position[0] = _position[0] - 1;
-                    break;
+                    if (_position[0] - 1 > 0)
+                    {
+                        _position[0] = _position[0] - 1;
+                        break;
+                    }   else 
+                        break;
                 case 3:
-                    _position[1] = _position[1] + 1;
-                    break;
+                    if (_position[1] + 1 < _fieldBorder[1] - 1)
+                    {
+                        _position[1] = _position[1] + 1;
+                        break;
+                    }   else
+                        break;
                 case 4:
-                    _position[1] = _position[1] - 1;
-                    break;
-            }
-            return _position;
+                    if (_position[1] - 1 > 0 )
+                    {
+                        _position[1] = _position[1] - 1;
+                        break;
+                    }   else
+                        break;
+            }   
+                return _position;
         }
     }
     public class Cyborg : Player
@@ -45,7 +69,11 @@
         {
             _position = _cyborgPosition;
             int directionSwitch;
-            if (playerPosition[0] < _position[0])
+            if (isHiden())
+            { 
+                directionSwitch = new Random().Next(1, 5);
+            }
+            else if (playerPosition[0] < _position[0])
             {
                  directionSwitch = 2;
                 
@@ -68,14 +96,40 @@
         }
 
     }
-    public class Tree 
+    public class Tree : Cyborg
     {
-        private int[] _treePosition = { new Random().Next(10, 15), new Random().Next(10, 15) };
-        public Tree()
+        public static int[] treePosition { get { return _fieldBorder; } set { _fieldBorder = value; } }
+        private static int[,] _forest = new int[treePosition[0], treePosition[1]];
+        public static int[,] forest { get { return _forest; } }
+        public static void forestGeneration()
         {
+            for (int i = 1; i < forest.GetLength(0)-1; i++)
+            {
           
-            string i = "ssss";
+                for (int j = 1; j < forest.GetLength(1)-1; j++)
+                {
+                    if ((i == playerPosition[0] && j == playerPosition[1]) 
+                        || 
+                        (i == cyborgPosition[0] && j == cyborgPosition[1]))
+                    {
+                        forest[i, j] = 0;
+                    }
+                    else
+                    forest[i, j] = new Random().Next(0, 2);
+                    
+                }
+            }
         }
-
+    }
+    public class Gem : Cyborg
+    {
+        private static int[] _gemPosition ={ new Random().Next(_fieldBorder[0] / 2, _fieldBorder[0] - 2), new Random().Next(_fieldBorder[1] / 2, _fieldBorder[1] - 2) };
+        public static int[] gemPosition { get { return _gemPosition; } }
+        public static int[] gMove()
+        {
+            _position = _gemPosition;
+            int directionSwitch = new Random().Next(1,5);
+            return Move(directionSwitch);
+        }
     }
 }
