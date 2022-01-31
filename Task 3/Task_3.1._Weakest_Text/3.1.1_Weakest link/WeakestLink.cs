25d69146d52fe70c3658b5e9079f9a37454cb7b6
Oleg <Kornyakovo@gmail.com>
2022-01-31 06:32:30 +0400

    Add: first task ("Weakest link")

---
+++ Task 3/Task_3.1._Weakest_Text/3.1.1_Weakest link/WeakestLink.cs
@@ -0,0 +1,73 @@
+﻿using used_frequently;
+
+namespace _3._1._1_Weakest_link
+{
+    public class WeakestLink
+    {
+        private static string[] PersonName = { "Yui", "Mio", "Ritsu", "Tsumugi", "Azusa"};
+        private static string[] PersonSurname = { "Hirasawa", "Akiyama", "Tainaka", "Kotobuki", "Nakano" };
+        private static List<string> persons = new List<string>();
+        private static int roundLength;
+        public void Main()
+        {
+            Console.Clear();
+            Console.Write("Circle length: ");
+            roundLength = UsedFrequently.Try2Parse();
+            CreateList(roundLength);
+            ListLog();
+            Round();
+        }
+        private static void CreateList(int roundLength)
+        {
+            
+            for (int i = 0; i < roundLength; i++)
+            {
+                persons.Add($"{PersonName[new Random().Next(0, PersonName.Length)]} {PersonSurname[new Random().Next(0, PersonSurname.Length)]}");
+            }
+        }
+        private static void Round()
+        {
+            Console.Write("\nEnter eluminated position: ");
+            int OutOnPosition = UsedFrequently.Try2ParseLessThenN(roundLength) - 1;
+
+                Console.Write("\nThe round has begun");
+            while (OutOnPosition <= persons.Count-1)
+            {
+                Console.WriteLine($"\nBye, {persons[OutOnPosition]} {persons.Count - 1} still in the game ");
+                persons.RemoveAt(OutOnPosition);
+
+            }
+            Console.WriteLine($"\nGame over\n--winners--");
+            ListLog();
+            Console.WriteLine("\n-----------");
+            Console.WriteLine("Press ENTER to start a new Round");
+                ConsoleKeyInfo cki;
+                do
+                {
+                    cki = Console.ReadKey(true);
+                    if (cki.Key == ConsoleKey.Enter)
+                    {
+                        persons.Clear();
+                        WeakestLink weakestList = new WeakestLink();
+                        weakestList.Main();
+                    }
+                } while (cki.Key != ConsoleKey.Escape);
+            
+        }
+        private static void ListLog()
+        {
+            if (persons.Count != 0)
+            {
+                foreach (string s in persons)
+                {
+                    Console.Write($"\n{s}");
+                }
+            }   else
+            {
+                Console.Write("\nno one...");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ Task 3/Task_3.1._Weakest_Text/Task_3.1._Weakest_Text/Program.cs
@@ -0,0 +1,35 @@
+﻿using used_frequently;
+using _3._1._1_Weakest_link;
+using System;
+namespace Program
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            NavigationBarConsole();
+            NavigationBarGoTo();
+        }
+        
+        private static void NavigationBarConsole()
+        {
+            Console.WriteLine("1. Weakest link");
+            Console.WriteLine("2. Text Analysis");
+        }
+        private static void NavigationBarGoTo()
+        {
+            int switcher = UsedFrequently.Try2ParseLessThenN(2);
+            switch (switcher)
+            {
+                case 1:
+                    WeakestLink weakestList = new WeakestLink();
+                    weakestList.Main();
+                    break;
+                case 2:
+                    Console.WriteLine("in process");
+                    Main();
+                    break;
+            }
+        }
+    }
+}
