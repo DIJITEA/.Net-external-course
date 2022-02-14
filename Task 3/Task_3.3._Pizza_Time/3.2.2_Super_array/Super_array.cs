160259096c9ae53d503bff6af6eed09407990cff
Oleg <Kornyakovo@gmail.com>
2022-02-14 06:11:35 +0400

    feat: add 3.3.1. Super array (without delegate)

---
+++ Task 3/Task_3.3._Pizza_Time/3.2.2_Super_array/Super_array.cs
@@ -0,0 +1,75 @@
+﻿namespace _3._2._2_Super_array
+{
+    public class Super_array
+    {
+        int[] SuperArray;
+        public Super_array()
+        {
+            SuperArray = new int[0];
+        }
+        public Super_array(int[] value)
+        {
+            SuperArray = new int[value.Length];
+            for (int i = 0; i < value.Length; i++)
+            {
+                SuperArray[i] = value[i];
+            }
+        }
+
+        public int Sum()
+        {
+            int temp = 0;
+            foreach (int item in this.SuperArray)
+            {
+                temp += item;
+            }
+            return temp;
+        }
+        public double Mean()
+        {
+            double result = Convert.ToDouble(this.SuperArray.Sum())/(this.SuperArray.Length);
+            return result;
+        }
+        public int closestToTheMean()
+        {
+            int result = 0;
+            double tempMean = Math.Round(Mean(), MidpointRounding.AwayFromZero); ;
+            double min = this.SuperArray.Max() - tempMean;
+            for (int i = 0; i < this.SuperArray.Length; i++)
+            {
+                if (this.SuperArray[i] - tempMean < min && this.SuperArray[i] - tempMean >= 0)
+                {
+                    result = this.SuperArray[i];
+                    min = result - tempMean;
+                }
+            }
+            return result;
+        }
+
+
+        public int MostFrequent()
+        {
+            Dictionary<int, int> tempDictionary = new Dictionary<int, int>();
+            for (int i = 0; i < this.SuperArray.Length; i++)
+            {
+                if (!tempDictionary.ContainsKey(this.SuperArray[i]))
+                {
+                    tempDictionary.Add(this.SuperArray[i], 1);
+                }
+                else tempDictionary[this.SuperArray[i]]++;
+            }
+            var MaxKV = (from e in tempDictionary select e.Value).Max();
+            foreach (var item in tempDictionary)
+            {
+                if (item.Value == MaxKV)
+                {
+                    return item.Key;
+                }
+            }
+            //var MaxV = MaxKV.Value;
+            return 0;
+        }
+
+
+    }
+}
---
+++ Task 3/Task_3.3._Pizza_Time/Task_3.3._Pizza_Time/Program.cs
@@ -0,0 +1,18 @@
+﻿using _3._2._2_Super_array;
+using System;
+
+namespace Program
+{
+    public class Program
+    { 
+        public static void Main()
+        {
+            int[] array1 = { 1, 2, 3, 4, 5, 6, 7, 7, 8, 9, 10, 11 };
+            Super_array TestArray = new Super_array(array1);
+            Console.WriteLine(TestArray.Sum());
+            Console.WriteLine(TestArray.MostFrequent());
+            Console.WriteLine(TestArray.Mean());
+            Console.WriteLine(TestArray.closestToTheMean());
+        }
+    }
+}
