c5428a2cee5588c632ae1b525d8b53c87ab0f2b7
Oleg <Kornyakovo@gmail.com>
2022-02-15 04:58:40 +0400

    feat: add 3.3.2. Super string (test mode)

---
+++ Task 3/Task_3.3._Pizza_Time/3.3.2._SUPER STRING/Super_string.cs
@@ -0,0 +1,57 @@
+﻿namespace _3._3._2._SUPER_STRING
+{
+    public class Super_string
+    {
+        public string stringReturn(string test)
+        {
+            int switcher = 0;
+            char[] stringChar = test.ToLower().ToCharArray();
+            foreach (char ch in stringChar)
+            {
+                if (ch != ' ')
+                {
+                    if( ch>= 'а' && ch <= 'я' && (switcher == 0 || switcher == 3))
+                    {
+                        switcher = 3;
+                    }
+                    else if(ch >= '0' && ch <= '9' && (switcher == 0 || switcher == 1) )
+                    {
+                        switcher = 1;
+                     
+                    }
+                        
+                    else if(ch >= 'a' && ch <= 'z' && (switcher == 0 || switcher == 2))
+                    { 
+                        switcher = 2;
+                    }
+                    else switcher = 4;
+                        
+                    Console.WriteLine((byte)ch);
+                }
+
+                
+            }
+            switch (switcher)
+            {
+                case 0: 
+                    return "empty";
+                    break;
+                case 1:
+                    return "number";
+                    break;
+                case 2:
+                    return "English";
+                    break;
+                case 3:
+                    return "Russian";
+                    break;
+                case 4:
+                    return "Mixed";
+                    break;
+
+            }
+            
+            return test;
+        }
+    }
+}
--- Task 3/Task_3.3._Pizza_Time/Task_3.3._Pizza_Time/Program.cs
+++ Task 3/Task_3.3._Pizza_Time/Task_3.3._Pizza_Time/Program.cs
@@ -1,4 +1,5 @@
 ﻿using _3._2._2_Super_array;
+using _3._3._2._SUPER_STRING;
 using System;
 
 namespace Program
@@ -8,11 +9,7 @@ namespace Program
         public static void Main()
         {
             int[] array1 = { 1, 2, 3, 4, 5, 6, 7, 7, 8, 8, 9, 10, 11 };
-            //Super_array TestArray = new Super_array(array1);
-            //Console.WriteLine(TestArray.Sum());
-            //Console.WriteLine(TestArray.MostFrequent());
-            //Console.WriteLine(TestArray.Mean());
-            //Console.WriteLine(TestArray.closestToTheMean());
+
             Super_array superArray = new Super_array();
             Super_array_Delegates.IntDelegate sum = new Super_array_Delegates.IntDelegate(superArray.Sum);
             Super_array_Delegates.DoubleDelegate mean = new Super_array_Delegates.DoubleDelegate(superArray.Mean);
@@ -22,6 +19,10 @@ namespace Program
             Console.WriteLine(mean(array1));
             Console.WriteLine(closestToTheMean(array1));
             Console.WriteLine(mostFrequent(array1));
+
+            Super_string str = new Super_string();
+            Console.WriteLine("-----");
+            Console.WriteLine(str.stringReturn(""));
         }
     }
 }
